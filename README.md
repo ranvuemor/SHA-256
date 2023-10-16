cd1de0051e2d374074bf844abb62a31b4a66d7d3
ranvuemor <amishkushwaha7@gmail>
2023-10-16 23:09:15 +0200

    added README.md

---
+++ README.md
@@ -0,0 +1,44 @@
+**SHA256 Hasing with Salt
+
+A simple implementation of SHA256 Hasing with Salt in C++
+
+***Example usage
+
+Run "./test":
+
+```
+$ ./test
+
+Welcome!!!
+==========
+Input to hash (type 'q' to quit): *****
+Hashed: 3a2cc518a1097664e7eb13710b6b058dc64df23d3d0c7be0bf276dd5b6699473
+
+To confirm: *****
+Hashed: 3a2cc518a1097664e7eb13710b6b058dc64df23d3d0c7be0bf276dd5b6699473
+
+Inputs matched!! You are good at remembering your passwords.
+
+Input to hash (type 'q' to quit): ***************
+Hashed: 787199f6441cd86e2f06fb10d5f053468a35c8501da3986b52eb8307bd85e27b
+
+To confirm: ***************
+Hashed: 787199f6441cd86e2f06fb10d5f053468a35c8501da3986b52eb8307bd85e27b
+
+Inputs matched!! You are good at remembering your passwords.
+
+Input to hash (type 'q' to quit): *****
+Hashed: 1fe347f3d21da5f11e375e7ada06877b0bc402eff6bc4b6705d9cb3f2ae5a3f1
+
+To confirm: *****
+Hashed: be206486a4f433e92c22d2a82d92c9f6e3fd5705e841079696869b76f6fddf5d
+
+Inputs didn't match!!
+
+Input to hash (type 'q' to quit): *
+
+Enjoy!!
+```
+
+
+
--- main.cpp
+++ main.cpp
@@ -27,16 +27,33 @@ char getch(void)
         perror("tcsetattr ~ICANON");
     //printf("%c\n", buf);
     return buf;
- }
+}
+
+string gen_random(const int len) {
+    static const char alphanum[] =
+        "0123456789"
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "abcdefghijklmnopqrstuvwxyz";
+    string tmp_s;
+    tmp_s.reserve(len);
+
+    for (int i = 0; i < len; ++i) {
+        tmp_s += alphanum[rand() % (sizeof(alphanum) - 1)];
+    }
+    
+    return tmp_s;
+}
 
 int main(){
-    cout << "Welcome!!!" << endl;
+    cout << endl  << "Welcome!!!" << endl << "==========" << endl;
     char ch;
     //const char ENTER = 13;
     bool ex = false;
     while (ex == false){
         string input1, input2, output1, output2;
         SHA256 in, check;
+        srand((unsigned)time(NULL) * getpid());  
+        string salt = gen_random(12);
         cout << "Input to hash (type 'q' to quit): ";
         ch = 0;
         while(ch != 10){
@@ -52,10 +69,12 @@ int main(){
             break;
         }
         else{
+            input1 = salt + input1;
             in.update(input1);
             uint8_t * digest1 = in.digest();
             output1 = SHA256::toString(digest1);
-            cout << "Hashed: " << output1 << endl;
+            //output1 = salt + output1;
+            cout << "Hashed: " << output1 << endl << endl;
             cout << "To confirm: ";
             ch = 0;
             while(ch != 10){
@@ -65,23 +84,25 @@ int main(){
                 }
                 ch = getch();
             }
+            input2 = salt + input2;
             cout << endl;
             check.update(input2);
             uint8_t * digest2 = check.digest();
             output2 = SHA256::toString(digest2);
-            cout << "Hashed: " << output2 << endl;
+            //output2 = salt + output2;
+            cout << "Hashed: " << output2 << endl << endl;
             if (output1 == output2){
-                cout << "Inputs matched!! You are good at remembering your passwords." << endl;
+                cout << "Inputs matched!! You are good at remembering your passwords." << endl << endl;
                 //cout << input1 << "  " << input2 << endl;
             }
             else {
-                cout << "Inputs didn't match!!" << endl;
+                cout << "Inputs didn't match!!" << endl << endl;
                 //cout << input1 << "  " << input2 << endl;
             }
             delete[] digest1;
             delete[] digest2;
         }
     }
-    cout << "Enjoy!!" << endl;
+    cout << endl << "Enjoy!!" << endl;
     return 0;
 }
